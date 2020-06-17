ba07c3089f71dc39a9a5dbaad67a75974e8a8995
jacobdewey <66138103+jacobdewey@users.noreply.github.com>
2020-06-17 11:45:28 -0400

    Update Y.ts

--- Y.ts
+++ Y.ts
@@ -1,7 +1,7 @@
 //% weight=100000000 color=#333333 icon="\uf234"
 namespace blockTest{
-//% blockId=when_do block="when  |%()=>void|%boolean"
-export function when(a: () => void, g: boolean){
+//% blockId=when_do block="when  %boolean"
+export function when(g: boolean, a: () => void){
 basic.forever(function () {
     
     if (g) {
