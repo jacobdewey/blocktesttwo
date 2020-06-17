e35d851b2b3e00d7479fd4ad1a80a00dea709646
jacobdewey <66138103+jacobdewey@users.noreply.github.com>
2020-06-17 11:36:21 -0400

    Update main.blocks, main.ts, Y.ts

--- Y.ts
+++ Y.ts
@@ -1,6 +1,6 @@
 //% weight=100000000 color=#333333 icon="\uf234"
 namespace blockTest{
-//% blockId=when_@g_do_@a block="when"
+//% blockId=when @g do @a block="when"
 export function when(a: () => void, g: boolean){
 basic.forever(function () {
     
