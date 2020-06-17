0e1056d35bf5c4027cfe48632db4acd8767687da
jacobdewey <66138103+jacobdewey@users.noreply.github.com>
2020-06-17 12:20:34 -0400

    Update Y.ts

--- Y.ts
+++ Y.ts
@@ -1,12 +1,12 @@
 //% weight=100000000 color=#333333 icon="\uf234"
 namespace moreBasic{
 //% blockId=when_do block="when  %boolean"
-export function when(g: boolean, a: () => void): () => void{
-return a
-    
+export function when(g: boolean, a: () => void): void{
+a
 }
 
 
 
 
 }
+
