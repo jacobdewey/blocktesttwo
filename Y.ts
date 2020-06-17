5c9adbb11daf6b39ff1af68b6408dfaa449ec223
jacobdewey <66138103+jacobdewey@users.noreply.github.com>
2020-06-17 11:23:59 -0400

    Update pxt.json, main.blocks, main.ts, Y.ts

---
+++ Y.ts
@@ -0,0 +1,13 @@
+//% weight=100000000 color=#333333 icon="\uf234"
+namespace blockTest{
+//% block
+export function blocktest(a: () => void, b: () => void, c: () => void): void{
+
+
+
+    
+}
+
+
+
+}
