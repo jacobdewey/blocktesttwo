5098dd637ef537b805a31b5dd8a24e314c656e62
jacobdewey <66138103+jacobdewey@users.noreply.github.com>
2020-06-17 11:27:28 -0400

    Update pxt.json, main.blocks, Y.ts

--- Y.ts
+++ Y.ts
@@ -1,9 +1,13 @@
 //% weight=100000000 color=#333333 icon="\uf234"
 namespace blockTest{
 //% block
-export function blocktest(a: () => void, b: () => void, c: () => void): void{
-
-
+export function when(a: () => void, g: boolean){
+basic.forever(function () {
+    
+    if (g) {
+        try {a} finally{}
+    }
+})
 
     
 }
